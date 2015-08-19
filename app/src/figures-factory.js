843154e115e210572493b48767820d0a8d202bf0
unknown <Illia_Shevchenko@EPUAKHAW0877.kyiv.epam.com>
2015-08-19 12:44:12 +0300

    1. Add figures-factory functionality

--- app/src/figures-factory.js
+++ app/src/figures-factory.js
@@ -1,14 +1,132 @@
 /**
  * Created by Illia_Shevchenko on 14.08.2015.
  */
+"use strict";
+
+
+/**
+ * A module for generating figures randomly
+ * @module figures-factory
+ */
 define(['figure'], function (Figure) {
-    var maps = [
-        [],
-        []
-    ];
+    var configurations = [
+            //LINE
+            [{
+                points: [0, 1,
+                         0, 1,
+                         0, 1,
+                         0, 1],
+                width: 2
+            }, {
+                points: [0, 0, 0, 0,
+                         1, 1, 1, 1],
+                width : 4
+            }],
+
+            //square
+            [{
+                points: [1, 1,
+                         1, 1],
+                width: 2
+            }],
+
+            //L
+            [{
+                points: [1, 0,
+                         1, 0,
+                         1, 1],
+                width : 2
+            }, {
+                points: [0, 0, 0,
+                         1, 1, 1,
+                         1, 0, 0],
+                width : 3
+            }],
+
+            //L reflected
+            [{
+                points: [0, 0, 1,
+                         0, 0, 1,
+                         0, 1, 1],
+                width : 3
+            }, {
+                points: [0, 0, 0,
+                         1, 0, 0,
+                         1, 1, 1],
+                width : 3
+            }],
+
+            //T
+            [{
+                points: [0, 0, 0,
+                         0, 1, 0,
+                         1, 1, 1],
+                width : 3
+            }, {
+                points: [1, 0,
+                         1, 1,
+                         1, 0],
+                width : 2
+            }, {
+                points: [1, 1, 1,
+                         0, 1, 0],
+                width : 3
+            }, {
+                points: [0, 0, 1,
+                         0, 1, 1,
+                         0, 0, 1],
+                width : 3
+            }],
+
+            //Z
+            [{
+                points: [0, 0, 0,
+                         1, 1, 0,
+                         0, 1, 1],
+                width : 3
+            }, {
+                points: [0, 0, 1,
+                         0, 1, 1,
+                         0, 1, 0],
+                width : 3
+            }],
+
+            //Z reflected
+            [{
+                points: [0, 0, 0,
+                         0, 1, 1,
+                         1, 1, 0],
+                width : 3
+            }, {
+                points: [1, 0,
+                         1, 1,
+                         0, 1],
+                width : 2
+            }]
+        ],
+        getRandom = function (max) {
+            return Math.floor(Math.random() * max);
+        };
+
     return {
-        getFigure: function () {
-            return new Figure({});
+        /**
+         * Returns Figure class instance with given position
+         * @param {number} left Left position for a figure
+         * @param {number} top Top position for a figure
+         * @returns {Figure}
+         */
+        getFigure: function (left, top) {
+            var configNumber = getRandom(configurations.length),
+                allowedConfiguration = configurations[configNumber],
+                startIndex = getRandom(allowedConfiguration.length);
+
+            return new Figure({
+                left: left,
+                top : top,
+                allowedConfigurations: allowedConfiguration,
+                startIndex : startIndex,
+                pointsValue: configNumber
+            });
         }
     };
 });
