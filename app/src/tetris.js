863bf96f6dc531f7a38028321f3bb761b047e22f
Illia Shevchenko <illyashevchenko@gmail.com>
2015-08-20 12:06:49 +0300

    1. Implement Tetris class. Tests passed

--- app/src/tetris.js
+++ app/src/tetris.js
@@ -11,16 +11,120 @@
  */
 define(function () {
     /**
+     * @global
+     * @typedef {Function} onNewFigure
+     * @returns {Figure}
+     */
+
+    /**
+     * @global
+     * @typedef {Function} onLineStrike
+     * @param {number} number Number of stricken off lines
+     */
+
+    /**
      * Creates instances of tetris game
+     * @param {object} settings Settings for tetris instance creation
+     * @param {Canvas} settings.canvas Canvas instance
+     * @param {Field} settings.field Field instance
+     * @param {onNewFigure} settings.onNewFigure Callback for getting new Figure
+     * @param {onLineStrike} [settings.onLineStrike] Callback on line strikes. Possible for scores
      * @class
      * @alias Tetris
      * @see module:tetris
      */
-    var Tetris = function () {
+    var Tetris = function (settings) {
+        this._canvas = settings.canvas;
+        this._field  = settings.field;
 
+        this._figure  = null;
+
+        this.onNewFigure = settings.onNewFigure;
+        this.onLineStrike = settings.onLineStrike;
     };
 
     Tetris.prototype = {
+        onLineStrike: function () {},
+
+        /**
+         * Sets new figure and adds it to the canvas
+         * @private
+         */
+        _setNewFigure: function () {
+            this._figure = this.onNewFigure();
+            this._canvas.addElement(this._figure.getMap());
+        },
+
+
+        /**
+         * Starts the game - adds field to canvas and set new figure
+         */
+        start: function () {
+            this._canvas.addElement(this._field.getMap());
+            this._setNewFigure();
+        },
+
+
+        /**
+         * Processes map with Tetris game logic
+         * @param {Map} map Map to process
+         * @private
+         */
+        _processMap: function (map) {
+            var strickenLines;
+
+            switch (this._field.checkMap(map)) {
+                case 1:
+                    this._figure.setMap(map);
+                    this._canvas.updateElement(map);
+                break;
+                case 2:
+                    this._canvas.removeElement(map);
+                    strickenLines = this._field.layMap(map);
+                    this.onLineStrike(strickenLines);
+                    this._canvas.updateElement(this._field.getMap());
+                    this._setNewFigure();
+                break;
+            }
+        },
+
+
+        /**
+         * Moves current figure down
+         */
+        down: function () {
+            var map = this._figure.moveDown();
+            this._processMap(map);
+        },
+
+
+        /**
+         * Moves current figure left
+         */
+        left: function () {
+            var map = this._figure.moveLeft();
+            this._processMap(map);
+        },
+
+
+        /**
+         * Moves current figure right
+         */
+        right: function () {
+            var map = this._figure.moveRight();
+            this._processMap(map);
+        },
+
+
+        /**
+         * Rotates current figure
+         */
+        rotate: function () {
+            var map = this._figure.rotate();
+            this._processMap(map);
+        },
+
+
         constructor: Tetris
     };
 
