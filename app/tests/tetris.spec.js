6e8d52abfd3270df08b0055d639b02605ec03bf4
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-08-26 17:24:27 +0300

    1. Fix Tetris to create new figures on the Field (not upper)

--- app/tests/tetris.spec.js
+++ app/tests/tetris.spec.js
@@ -94,7 +94,8 @@ define(['tetris'], function (Tetris) {
 
                 expect(settings.canvas.getSizes).toHaveBeenCalled();
                 expect(figure.setOffsetPosition).toHaveBeenCalledWith({
-                    x: 10
+                    x: 10,
+                    y: 1
                 });
                 expect(settings.canvas.addElement).toHaveBeenCalledWith(figureMap);
             });
