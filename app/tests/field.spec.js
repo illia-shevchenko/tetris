9f5f111d9677ca6ee801f9f7989e3b553bb43a1a
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-08-27 11:43:44 +0300

    1. Fix test for field to have to pseparate methods for lay figure and check it

--- app/tests/field.spec.js
+++ app/tests/field.spec.js
@@ -32,72 +32,82 @@ define(['field'], function (Field) {
 
         describe('Check maps with empty field', function () {
             it('should successfully check maps that suits free space', function () {
-                var result = field.checkMap({
+                var map = {
                     left: 0,
                     top : 0,
                     points: [
                         0, 0, 0, 0,
                         2, 2, 2, 2],
                     width : 4
-                });
+                };
 
-                expect(result).toBe(1);
+                expect(field.checkOverlay(map)).toBe(true);
+                expect(field.checkLay(map)).toBe(false);
+                expect(field.checkOverSize(map)).toBe(false);
             });
 
             it('should successfully check maps that have unsuitable sizes but internally suits free space', function () {
-                var result = field.checkMap({
+                var map = {
                     left: 2,
                     top : 0,
                     points: [
                         0, 0, 0, 0,
                         0, 2, 2, 0],
                     width : 4
-                });
+                };
 
-                expect(result).toBe(1);
+                expect(field.checkOverlay(map)).toBe(true);
+                expect(field.checkLay(map)).toBe(false);
+                expect(field.checkOverSize(map)).toBe(false);
             });
 
             it('should reject maps with negative left position', function () {
-                var result = field.checkMap({
+                var map = {
                     left: -1,
                     top : 0,
                     points: [
                         0, 0, 0, 0,
                         2, 2, 2, 2],
                     width : 4
-                });
+                };
 
-                expect(result).toBe(0);
+                expect(field.checkOverlay(map)).toBe(false);
+                expect(field.checkLay(map)).toBe(false);
+                expect(field.checkOverSize(map)).toBe(true);
             });
 
             it('should successfully check maps with negative left position but internally suits free space', function () {
-                var result = field.checkMap({
+                var map = {
                     left: -1,
                     top : 0,
                     points: [
                         0, 0, 0, 0,
                         0, 2, 2, 0],
                     width : 4
-                });
+                };
 
-                expect(result).toBe(1);
+                expect(field.checkOverlay(map)).toBe(true);
+                expect(field.checkLay(map)).toBe(false);
+                expect(field.checkOverSize(map)).toBe(false);
             });
 
             it('should reject maps unsuitable by top position', function () {
-                var result = field.checkMap({
+                var map = {
                     left: 0,
                     top : 4,
                     points: [
                         0, 2, 0, 0,
                         2, 2, 2, 2],
                     width : 4
-                });
+                };
 
-                expect(result).toBe(0);
+                expect(field.checkOverlay(map)).toBe(false);
+                expect(field.checkLay(map)).toBe(false);
+                expect(field.checkOverSize(map)).toBe(true);
             });
 
             it('should detect map to be laid to the empty field', function () {
-                var result = field.checkMap({
+                var map = {
                     left: 0,
                     top : 3,
                     points: [
@@ -105,9 +115,30 @@ define(['field'], function (Field) {
                         0, 2, 2, 0,
                         0, 0, 0, 0],
                     width : 4
-                });
+                };
 
-                expect(result).toBe(-1);
+                expect(field.checkOverlay(map)).toBe(true);
+                expect(field.checkLay(map)).toBe(true);
+                expect(field.checkOverSize(map)).toBe(false);
+            });
+
+            it('should not detect map to be laid if any slice of it is above the field', function () {
+                var map = {
+                    left: 0,
+                    top : -1,
+                    points: [
+                        0, 1,
+                        0, 1,
+                        0, 1,
+                        0, 1,
+                        0, 1,
+                        0, 1],
+                    width : 2
+                };
+
+                expect(field.checkOverlay(map)).toBe(true);
+                expect(field.checkLay(map)).toBe(true);
+                expect(field.checkOverSize(map)).toBe(true);
             });
         });
 
@@ -138,7 +169,7 @@ define(['field'], function (Field) {
 
             describe('Not empty field', function () {
                 it('should reject maps which does not suit not empty field', function () {
-                    var result = field.checkMap({
+                    var map = {
                         left: 0,
                         top : 1,
                         points: [
@@ -146,9 +177,11 @@ define(['field'], function (Field) {
                             0, 3, 3, 0,
                             0, 3, 3, 0],
                         width : 4
-                    });
+                    };
 
-                    expect(result).toBe(0);
+                    expect(field.checkOverlay(map)).toBe(false);
+                    expect(field.checkLay(map)).toBe(false);
+                    expect(field.checkOverSize(map)).toBe(false);
                 });
 
                 it('should detect map to be laid and lay map properly on not empty field', function () {
@@ -161,12 +194,13 @@ define(['field'], function (Field) {
                             0, 3, 3, 0,
                             0, 0, 0, 0],
                         width : 4
-                    },
-                        result = field.checkMap(map);
+                    };
 
-                    expect(result).toBe(-1);
+                    expect(field.checkOverlay(map)).toBe(true);
+                    expect(field.checkLay(map)).toBe(true);
+                    expect(field.checkOverSize(map)).toBe(false);
+
                     field.layMap(map);
-
                     expect(field.getMap().points).toEqual([
                         0, 0, 0, 0, 0,
                         0, 0, 0, 0, 0,
@@ -186,10 +220,11 @@ define(['field'], function (Field) {
                             0, 3, 0, 3, 0,
                             0, 0, 0, 3, 0],
                         width : 5
-                    },
-                        result = field.checkMap(map);
+                    };
 
-                    expect(result).toBe(-1);
+                    expect(field.checkOverlay(map)).toBe(true);
+                    expect(field.checkLay(map)).toBe(true);
+                    expect(field.checkOverSize(map)).toBe(false);
                     field.layMap(map);
 
                     expect(field.getMap().points).toEqual([
@@ -237,12 +272,13 @@ define(['field'], function (Field) {
                                 0, 0, 0, 3, 0],
                             width : 5
                         },
-                        result = field.checkMap(map),
                         lines;
 
-                    expect(result).toBe(-1);
-                    lines = field.layMap(map);
+                    expect(field.checkOverlay(map)).toBe(true);
+                    expect(field.checkLay(map)).toBe(true);
+                    expect(field.checkOverSize(map)).toBe(false);
 
+                    lines = field.layMap(map);
                     expect(lines).toBe(1);
                     expect(field.getMap().points).toEqual([
                         0, 0, 0, 0, 0,
