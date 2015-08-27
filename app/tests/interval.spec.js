c7511b413cbe8d0080beffa3e91e945cecb2ae96
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-08-27 15:42:28 +0300

    1. Add tests for Interval class

--- app/tests/interval.spec.js
+++ app/tests/interval.spec.js
@@ -12,32 +12,84 @@ define(['interval'], function (Interval) {
         beforeEach(function () {
             callback = jasmine.createSpy('callback');
 
-            interval = new Interval();
+            interval = new Interval(callback, 5);
             jasmine.clock().install();
+            interval.start();
         });
 
         afterEach(function () {
             jasmine.clock().uninstall();
+            callback.calls.reset();
         });
 
         it('should starts timer', function () {
+            expect(callback).not.toHaveBeenCalled();
+            jasmine.clock().tick(6);
 
+            expect(callback).toHaveBeenCalled();
+        });
+
+        it('should starts timer only once', function () {
+            interval.start();
+            jasmine.clock().tick(8); // for 8 ms it should not be called twice
+            expect(callback.calls.count()).toBe(1);
+        });
+
+        it('should be able to change interval time and apply it', function () {
+            jasmine.clock().tick(6);
+            expect(callback.calls.count()).toBe(1);
+
+            interval.set(10);
+
+            jasmine.clock().tick(15);
+            expect(callback.calls.count()).toBe(2);
         });
 
         it('should be able to pause timer', function () {
+            interval.pause();
+            jasmine.clock().tick(6);
+            expect(callback).not.toHaveBeenCalled();
 
+            jasmine.clock().tick(13);
+            expect(callback).not.toHaveBeenCalled();
         });
 
         it('should be able to resume timer after pausing timer', function () {
+            interval.pause();
+            interval.pause();
 
+            jasmine.clock().tick(6);
+            expect(callback).toHaveBeenCalled();
         });
 
         it('should be able to stop timer', function () {
+            interval.stop();
 
+            jasmine.clock().tick(6);
+            expect(callback).not.toHaveBeenCalled();
         });
 
         it('should not allow to resume timer after stopping', function () {
+            interval.stop();
 
+            interval.pause();
+            jasmine.clock().tick(6);
+            expect(callback).not.toHaveBeenCalled();
+
+            interval.pause();
+            jasmine.clock().tick(6);
+            expect(callback).not.toHaveBeenCalled();
+        });
+
+        it('should be able to restart timer after stopping', function () {
+            jasmine.clock().tick(6);
+            interval.stop();
+
+            jasmine.clock().tick(6);
+            interval.start();
+
+            jasmine.clock().tick(6);
+            expect(callback).toHaveBeenCalled();
         });
     });
 });
