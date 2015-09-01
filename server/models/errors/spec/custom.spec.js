b82beac3f705755526a87198e108116501c38687
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-09-01 13:09:46 +0300

    1. Add test spec for CustomError. Need to make it works because it resist starting on pure io.js

---
+++ server/models/errors/spec/custom.spec.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Illia_Shevchenko on 01.09.2015.
+ */
+'use strict';
+
+let CustomError = require('../custom');
+
+describe('CustomError', function () {
+    it('should provide default parameters', function () {
+        let custom = new CustomError('message');
+
+        expect(custom.errorMessage).toEqual('message');
+        expect(custom.data).toEqual('');
+        expect(custom.type).toEqual('unknown');
+        expect(custom.status).toBe(400);
+
+        //expect(custom.toString()).toEqual('');
+    });
+});
