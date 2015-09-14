06a58d49133f4002258486cdb3c034e863aec4e2
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-09-14 14:21:38 +0300

    1. Fix mockgoose test helper - add global method for mocking mongoose for unit test 2. Comment out not working code for integrational test

--- server/tests/helpers/mockgoose.js
+++ server/tests/helpers/mockgoose.js
@@ -4,7 +4,13 @@
 'use strict';
 
 
-import mongoose from 'mongoose';
-import mockgoose from 'mockgoose';
+//import mongoose from 'mongoose';
+//import mockgoose from 'mockgoose';
+import mongooseMock from 'mongoose-mock';
+import proxyquire from 'proxyquire';
 
-mockgoose(mongoose);
+//mockgoose(mongoose);
+
+global.proxyquire = function (module) {
+    return proxyquire(module, { mongoose: mongooseMock });
+};
