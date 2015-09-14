11a99abcbbb0adfe3419d3184a1d68c1bad12448
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-09-14 16:00:52 +0300

    1. Add some code to game.spec.js and comment it out

--- server/models/game/spec/game.spec.js
+++ server/models/game/spec/game.spec.js
@@ -2,3 +2,18 @@
  * Created by Illia_Shevchenko on 28.08.2015.
  */
 'use strict';
+/*eslint max-nested-callbacks: 0*/
+/*global proxyquire, sinon*///see helpers/
+
+
+describe('Game model', () => {
+    var Game;
+    before(() => {
+        Game = proxyquire('../');
+    });
+
+    describe('create and save', function () {
+        let callback = sinon.spy();
+        //Game.create
+    });
+});
