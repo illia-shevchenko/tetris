191f17b44b26ab86eb50b3e06180b201498aed39
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-09-16 17:46:57 +0300

    1. Add some mock upgrading to mockgoose helper 2. Use it and add tests for testing game update and deletion

--- server/models/game/spec/game.spec.js
+++ server/models/game/spec/game.spec.js
@@ -3,17 +3,77 @@
  */
 'use strict';
 /*eslint max-nested-callbacks: 0*/
-///*global proxyquire, sinon*///see helpers/
+
+import mongooseMock from '../../../tests/helpers/mockgoose.js'; //there are some additional manipulations on it
+import proxyquire from 'proxyquire';
 
 
 describe('Game model', () => {
-    //var Game;
-    before(() => {
-        //Game = proxyquire('../');
+    var Game;
+
+    beforeEach(() => {
+        Game = proxyquire('../../game', { mongoose: mongooseMock });
     });
 
-    describe('create and save', function () {
-        //let callback = sinon.spy();
-        //Game.create
+    describe('create and save', () => {
+    });
+
+
+    describe('remove by id', () => {
+        afterEach(() => {
+            Game.remove.reset();
+        });
+
+        it('should find documents to remove', function () {
+            Game.removeById('coolId');
+            //using expect(Game.remove).calledOnce make both WebStorm and ESLint crazy
+            expect(Game.remove.calledOnce).to.equal(true);
+            expect(Game.remove.getCall(0).args[0]).to.containSubset({
+                _id: 'coolId'
+            });
+        });
+    });
+
+    describe('update by id', () => {
+        let newGame = {
+            nextFigure: {
+                top: 1,
+                left: 1,
+                width: 1,
+                points: [0]
+            },
+            figure: {
+                top: 2,
+                left: 2,
+                width: 2,
+                points: [0, 0]
+            },
+            field: {
+                top: 0,
+                left: 0,
+                width: 10,
+                points: new Array(10).fill(1)
+            },
+            score: 10,
+            user : 'tester'
+        };
+
+        afterEach(() => {
+            Game.findOne.reset();
+        });
+
+        it('should find document, update it and save', (done) => {
+            Game.updateById('coolId', newGame)
+                .then((game) => {
+                    expect(Game.findOne.calledOnce).to.equal(true);
+                    expect(Game.findOne.getCall(0).args[0]).to.containSubset({
+                        _id: 'coolId'
+                    });
+
+                    expect(game).to.containSubset(newGame);
+                    expect(game.save.calledOnce).to.equal(true);
+                    done();
+                });
+        });
     });
 });
--- server/tests/helpers/mockgoose.js
+++ server/tests/helpers/mockgoose.js
@@ -6,10 +6,53 @@
 
 //import mongoose from 'mongoose';
 //import mockgoose from 'mockgoose';
-//import mongooseMock from 'mongoose-mock';
 //import proxyquire from 'proxyquire';
 
 //mockgoose(mongoose);
 
-//global.proxyquire = proxyquire;
-//global.mongooseMock = { mongoose: mongooseMock };
+
+import mongooseMock from 'mongoose-mock';
+import sinon from 'sinon';
+
+
+/**
+ * Add mocks which is not existed in the mongooseMock v0.4.0
+ */
+let oldSchema = mongooseMock.Schema,
+
+    getPromisedStub = (Obj) => {
+        let result = Obj,
+            stub = () => {
+                return Promise.resolve(result);
+            };
+
+        if (typeof Obj === 'function') {
+            result = new Obj();
+        }
+
+        return stub;
+    };
+
+mongooseMock.Schema = () => {
+    let oldMock = oldSchema(),
+        Mock = function () {
+            /**
+             * We need to redefine some logic in constructor
+             */
+            oldMock.apply(this, arguments);
+
+            this.save = sinon.stub().returns(this);
+        };
+
+    //Copy all static properties
+    Object.assign(Mock, oldMock);
+
+    Mock.findOne = getPromisedStub(Mock);
+    sinon.spy(Mock, 'findOne');
+
+    Mock.eachPath = sinon.stub();
+
+    return Mock;
+};
+
+export default mongooseMock;
