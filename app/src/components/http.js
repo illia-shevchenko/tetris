6c72adbb3597890de4adc0b246f15b34274cd536
Illia Shevchenko <Illia_Shevchenko@epam.com>
2015-09-18 11:29:37 +0300

    1. Add http vodule. Need to add tests to it

--- app/src/components/http.js
+++ app/src/components/http.js
@@ -4,12 +4,89 @@
 'use strict';
 
 
-define(['lodash', 'bluebird'], function (_, bluebird) {
+/**
+ * A module representing http factory to create http calls
+ * @module http
+ */
+define(['lodash', 'bluebird'], function (_, Promise) {
+    /**
+     * Creates http cal with given parameters
+     * @param {Object} settings Settings for call
+     * @param {string} settings.url Url to call
+     * @param {string} settings.method Http method - 'GET', 'POST' etc
+     * @param {Object} settings.headers SObject where key is header name and value is i's value
+     * @param {string} settings.user User name for a call
+     * @param {string} settings.password Password for the call
+     * @returns {Promise} Promise for request. Cancelable.
+     * @see https://github.com/petkaantonov/bluebird/blob/master/API.md#cancellable---promise
+     * @alias module:http
+     */
     var http = function (settings) {
-            console.log(typeof bluebird);
-            console.log(settings);
+            var xhr = new XMLHttpRequest();
+
+            _.assign({
+                headers: {},
+                method : 'GET',
+                url    : 'http://localhost'
+            }, settings);
+
+
+            setHeaders(xhr, settings.headers);
+            xhr.open(settings.method, settings.url, true, settings.user, settings.password);
+
+            return new Promise(function (resolve, reject) {
+                /**
+                 * Creates response data object
+                 * @returns {{responseHeaders: *, statusText: *, response, request: Object, status: *}}
+                 */
+                var getResponseData = function () {
+                    try {
+                        return {
+                            responseHeaders: getResponseHeaders(xhr),
+                            statusText     : this.statusText,
+
+                            response: JSON.parse(this.response),
+                            request : settings,
+                            status  : this.status
+                        };
+                    } catch (error) {
+                        reject(error);
+                    }
+                };
+
+                xhr.addEventListener('error timeout', function () {
+                    reject(getResponseData.apply(this));
+                });
+
+                xhr.addEventListener('load', function () {
+                    resolve(getResponseData.apply(this));
+                });
+
+                xhr.send(settings.data);
+            })
+                .cancellable()
+                .catch(Promise.CancellationError, function (error) {
+                    xhr.abort();
+                    return Promise.reject(error);
+                });
         },
 
+
+        /**
+         * @typedef {Object} http~config Configuration object for http shortcuts
+         * @property {Object} headers Object where key is header name and value is i's value
+         * @property {string} settings.user User name for a call
+         * @property {string} settings.password Password for the call
+         */
+
+
+        /**
+         * Method for creating shortcuts with body
+         * @param {string} url Url to call
+         * @param {string} method Http method - 'GET', 'POST' etc
+         * @param {Object} data Data to set as request body
+         * @param {http~config} config
+         */
         httpMethodWithBody = function (method, url, data, config) {
             var settings = _.assign({}, config, {
                 method: method,
@@ -20,14 +97,94 @@ define(['lodash', 'bluebird'], function (_, bluebird) {
             return http(settings);
         },
 
+
+        /**
+         * Method for creating shortcuts without body
+         * @param {string} url Url to call
+         * @param {string} method Http method - 'GET', 'POST' etc
+         * @param {http~config} config
+         */
         httpMethod = function (method, url, config) {
-            httpMethodWithBody(method, url, null, config);
+            return httpMethodWithBody(method, url, null, config);
+        },
+
+
+        /**
+         * Sets headers to xhr
+         * @param {XMLHttpRequest} xhr Xhr object set headers to
+         * @param {Object} headers Object where key is header name and value is i's value
+         */
+        setHeaders = function (xhr, headers) {
+            Object.keys(headers).forEach(function (key) {
+                xhr.setRequestHeader(key, headers[key]);
+            });
+        },
+
+
+        /**
+         * Gets response headers from xhr
+         * @param {XMLHttpRequest} xhr Xhr object set headers from
+         * @returns {Object} Object where key is header name and value is i's value
+         */
+        getResponseHeaders = function (xhr) {
+            return xhr.getAllResponseHeaders()
+                .split('\r\n')
+                .reduce(function (headers, header) {
+                    var headerArr = header.split(': '),
+                        key = headerArr[0];
+
+                    if (key) {
+                        headers[key] = headerArr[1];
+                    }
+                    return headers;
+                }, {});
         };
 
-    http.get       = httpMethod.bind(null, 'GET');
+
+    /**
+     * Shortcut method for 'GET' request
+     * @function
+     * @param {string} url Url to call
+     * @param {Object} [params] Parameters for call
+     * @returns {Promise} Promise for request
+     */
+    http.get       = httpMethod.bind(http, 'GET');
+
+    /**
+     * Shortcut method for 'DELETE' request
+     * @method delete
+     * @param {string} url Url to call
+     * @param {Object} [params] Parameters for call
+     * @returns {Promise} Promise for request
+     */
     http['delete'] = httpMethod.bind(http, 'DELETE');
+
+    /**
+     * Shortcut method for 'PUT' request
+     * @function
+     * @param {string} url Url to call
+     * @param {Object} [params] Parameters for call
+     * @returns {Promise} Promise for request
+     */
     http.put       = httpMethod.bind(http, 'PUT');
+
+    /**
+     * Shortcut method for 'HEAD' request
+     * @function
+     * @param {string} url Url to call
+     * @param {Object} [params] Parameters for call
+     * @returns {Promise} Promise for request
+     */
     http.head      = httpMethod.bind(http, 'HEAD');
+
+    /**
+     * Shortcut method for 'POST' request
+     * @function
+     * @param {string} url Url to call
+     * @param {Object} data Data to set as request body
+     * @param {Object} [params] Parameters for call
+     * @returns {Promise} Promise for request
+     */
     http.post      = httpMethodWithBody.bind(http, 'POST');
 
     return http;
